1e8bef1fb2bee067770f0b71f426dcc769be06e5
anthonywww <31651690+anthonywww@users.noreply.github.com>
2018-01-23 11:37:51 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# This repository is dedicated to a introduction to programming in Java course
---
+++ src/com/github/anthonywww/lab1/Hello.java
@@ -0,0 +1,25 @@
+package com.github.anthonywww.lab1;
+
+/**
+ * Hello World Java test program
+ *
+ * @author Anthony Waldsmith
+ * @version 1/23/2018
+ */
+public class Hello {
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+}
+
+/* Part B
+ * 1. Hello.java:8: error: '{' expected
+ * 2. Hello.java:8: error: reached end of file while parsing
+ * 3. Hello.java:10: error: reached end of file while parsing
+ * 4. Hello.java:10: error: class, interface, or enum expected
+ * 5. Hello.java:10: error: ';' expected
+ * 6. 
+ * 7. Hello.java:10: error: ';' expected
+ * 8. Hello.java:10: error: cannot find symbol
+ * 9. Hello.java:10: error: cannot find symbol
+ */

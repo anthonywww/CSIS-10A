bc181724b3b539ffdf158a49b28f4a86c89249f7
anthonywww <31651690+anthonywww@users.noreply.github.com>
2018-04-03 12:31:53 -0700

    Updted CircleBugRunner

--- src/com/github/anthonywww/lab10/CircleBugRunner.java
+++ src/com/github/anthonywww/lab10/CircleBugRunner.java
@@ -32,11 +32,9 @@ public class CircleBugRunner {
 	
 	public static void main(String[] args) {
 		ActorWorld world = new ActorWorld();
-		BoxBug alice = new BoxBug(6);
+		CircleBug alice = new CircleBug(3);
 		alice.setColor(Color.ORANGE);
-		BoxBug bob = new BoxBug(3);
-		world.add(new Location(7, 8), alice);
-		world.add(new Location(5, 5), bob);
+		world.add(new Location(4, 4), alice);
 		world.show();
 	}
 	
